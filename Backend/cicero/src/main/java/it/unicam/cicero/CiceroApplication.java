c8b18b9c9a80ddf801935be2464aee892cb2edfc
Chiara Cintioni <83300887+chiara-cintioni@users.noreply.github.com>
2021-12-07 16:07:15 +0100

    Add files via upload

---
+++ Backend/cicero/src/main/java/it/unicam/cicero/CiceroApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cicero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiceroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiceroApplication.class, args);
+	}
+
+}
